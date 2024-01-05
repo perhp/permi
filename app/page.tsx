d498a5a0873a08d72c25fc30056291f8210c1f23
Per Hansen <me@perhp.com>
2024-01-05 22:32:47 +0100

    show latest pass

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
-export default function Page() {
-  return <main className="container">Hello</main>;
+import { CDN_URL } from "@/lib/cdn-url";
+import { supabaseServiceClient } from "@/lib/supabase";
+import { format } from "date-fns";
+
+export default async function Page() {
+  const { data: passes } = await supabaseServiceClient
+    .from("passes")
+    .select(
+      `id, created_at, pass_start, daylight_pass, is_noaa, 
+      gain, has_pristine, has_polar_az_el, has_polar_direction, 
+      has_histogram, is_meteor, has_spectrogram,
+      images:passes_images(id, created_at, path)`
+    )
+    .order("created_at", { ascending: false });
+
+  if (!passes) {
+    throw new Error("No passes found");
+  }
+
+  const [latestPass] = passes;
+  const { images } = latestPass;
+  const [satelliteIdentifier, satelitteNumber] = images[0].path.split("-");
+  const satelliteName = latestPass.is_noaa ? `${satelliteIdentifier} ${satelitteNumber}` : "Meteor M2-3";
+
+  return (
+    <main className="container py-20">
+      <h1 className="text-sm text-gray-600">{format(latestPass.pass_start, "dd. MMM @ HH:mm")}</h1>
+      <h2 className="text-4xl font-bold">{satelliteName}</h2>
+      <div className="grid grid-cols-5 gap-5 mt-5">
+        {images.map((image) => (
+          <img key={image.id} src={`${CDN_URL}/images/${image.path}`} alt="" className="rounded-md" />
+        ))}
+      </div>
+    </main>
+  );
 }
---
+++ lib/cdn-url.ts
@@ -0,0 +1,1 @@
+export const CDN_URL = "https://htyouocguzgigopxxidt.supabase.co/storage/v1/object/public/passes";
