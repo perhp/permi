810594143560affd9fe5268fbb217efd7cd25f57
Per Hansen <me@perhp.com>
2024-01-05 22:04:44 +0100

    fix header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,19 +3,21 @@ import { NavLink } from "./nav-link";
 
 export default function Header() {
   return (
-    <header className="flex items-center border-b border-gray-100 justify-between">
-      <div className="flex items-center font-bold p-5 gap-1.5">
-        <SatelliteDish className="w-5 h-5" /> permi
+    <header className="border-b border-gray-100">
+      <div className="flex items-center justify-between container h-14">
+        <div className="flex items-center font-bold gap-1.5">
+          <SatelliteDish className="w-5 h-5" /> <span className="hidden sm:inline">permi</span>
+        </div>
+        <nav className="pr-5">
+          <ul className="flex gap-5 items-center">
+            <NavLink href="/">Latest pass</NavLink>
+            <NavLink href="/all-passes">All passes</NavLink>
+            <NavLink href="https://github.com/perhp/permi" target="_blank" className="bg-gray-100 hover:bg-gray-100/75 p-2 rounded-full">
+              <Github className="w-5 h-5" />
+            </NavLink>
+          </ul>
+        </nav>
       </div>
-      <nav className="pr-5">
-        <ul className="flex gap-5 items-center">
-          <NavLink href="/">Latest pass</NavLink>
-          <NavLink href="/all-passes">All passes</NavLink>
-          <NavLink href="https://github.com/perhp/permi" target="_blank" className="bg-gray-100 hover:bg-gray-100/75 p-2 rounded-full">
-            <Github className="w-5 h-5" />
-          </NavLink>
-        </ul>
-      </nav>
     </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
-  return <main className="flex flex-col items-center justify-between p-24"></main>;
+export default function Page() {
+  return <main className="container">Hello</main>;
 }
