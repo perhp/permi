ef605260baa05a58d85c6295d066317336965fa8
Per Hansen <me@perhp.com>
2024-01-12 11:43:47 +0100

    fix gap

--- app/_components/pass.tsx
+++ app/_components/pass.tsx
@@ -46,7 +46,7 @@ export default function Pass({ pass }: Props) {
 
   return (
     <>
-      <div className="flex flex-col gap-2 md:flex-row">
+      <div className="flex flex-col gap-2 md:gap-5 md:flex-row">
         <h1 className="text-sm font-medium text-gray-500">
           {format(pass_start, "dd. MMM @ HH:mm")} <br />
           <span className="text-4xl font-bold text-black">{satelliteName}</span>
