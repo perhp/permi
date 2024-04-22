9fbb6deac4f5f8f6389aace414137e000a588e87
Per Hansen <me@perhp.com>
2024-04-22 20:51:46 +0200

    feat: get new properties when getting pass

--- queries/pass.query.ts
+++ queries/pass.query.ts
@@ -1,4 +1,5 @@
 export const passQuery = `id, created_at, pass_start, daylight_pass, is_noaa, 
     gain, has_pristine, has_polar_az_el, has_polar_direction, 
-    has_histogram, is_meteor, has_spectrogram,
+    has_histogram, is_meteor, has_spectrogram, max_elevation, 
+    direction, azimuth_at_max, pass_end, pass_start_azimuth,
     images:passes_images(id, created_at, path)`;
