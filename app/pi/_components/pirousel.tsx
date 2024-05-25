bcea88325a0fb946d6b56c5aec1285f6b9d54e9e
Per Hansen <me@perhp.com>
2024-05-25 16:38:46 +0200

    feat: add controls to pirousel

--- app/pi/_components/pirousel.tsx
+++ app/pi/_components/pirousel.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+import { Carousel, CarouselApi, CarouselContent, CarouselItem } from "@/components/ui/carousel";
 import { CDN_URL } from "@/lib/cdn-url";
 import { Pass } from "@/models/pass.model";
 import { getSatelitteName } from "@/utils/get-satellite-name";
 import { format } from "date-fns";
 import Autoplay from "embla-carousel-autoplay";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type Props = {
   latestPass: Pass;
 };
 
+export const revalidate = 0;
 export default function Pirousel({ latestPass }: Props) {
   const router = useRouter();
+  const [carousel, setCarousel] = useState<CarouselApi>();
 
   useEffect(() => {
     const refresh = setInterval(() => {
@@ -26,9 +30,18 @@ export default function Pirousel({ latestPass }: Props) {
 
   const satelliteName = getSatelitteName(latestPass);
 
+  const next = () => {
+    carousel?.scrollNext();
+  };
+
+  const previous = () => {
+    carousel?.scrollPrev();
+  };
+
   return (
     <>
       <Carousel
+        setApi={setCarousel}
         opts={{
           loop: true,
           duration: 100,
@@ -44,9 +57,36 @@ export default function Pirousel({ latestPass }: Props) {
           ))}
         </CarouselContent>
       </Carousel>
-      <div className="fixed z-20 bottom-5 inset-x-5 border text-white p-5 border-white/5 bg-white/5 rounded-lg backdrop-blur">
-        <p className="text-white/60 text-sm">{format(latestPass.pass_start, "dd. MMM @ HH:mm")}</p>
-        <p className="text-lg font-medium">{satelliteName}</p>
+      <div className="fixed z-20 bottom-5 inset-x-5 border text-white border-white/5 bg-white/5 rounded-lg backdrop-blur">
+        <div className="border-b border-white/5 py-3 flex">
+          <button onClick={previous} className="h-full flex items-center p-3">
+            <ChevronLeftIcon className="w-4 h-4" />
+          </button>
+          <div className="flex flex-col w-full items-center">
+            <p className="text-white/60 text-xs">{format(latestPass.pass_start, "dd. MMM @ HH:mm")}</p>
+            <p className="font-medium">{satelliteName}</p>
+          </div>
+          <button onClick={next} className="h-full flex items-center p-3">
+            <ChevronRightIcon className="w-4 h-4" />
+          </button>
+        </div>
+        <div className="flex">
+          <Link
+            href={{ pathname: "/pi", query: { series: "noaa" } }}
+            className="font-light text-xs flex-1 p-3 border-r border-white/5 text-center"
+          >
+            Latest NOAA
+          </Link>
+          <Link
+            href={{ pathname: "/pi", query: { series: "meteor" } }}
+            className="border-r border-white/5 font-light text-xs flex-1 p-3 text-center"
+          >
+            Latest Meteor
+          </Link>
+          <Link href={{ pathname: "/pi" }} className="font-light text-xs flex-1 p-3 text-center">
+            Latest pass
+          </Link>
+        </div>
       </div>
     </>
   );
--- app/pi/page.tsx
+++ app/pi/page.tsx
@@ -2,8 +2,22 @@ import { supabaseServiceClient } from "@/lib/supabase";
 import { passQuery } from "@/queries/pass.query";
 import Pirousel from "./_components/pirousel";
 
-export default async function Page() {
-  const { data } = await supabaseServiceClient.from("passes").select(passQuery).order("created_at", { ascending: false }).limit(1);
+type Props = {
+  searchParams: { [key: string]: string[] | string | undefined };
+};
+
+export default async function Page({ searchParams }: Props) {
+  const passesBuilder = supabaseServiceClient.from("passes").select(passQuery).order("created_at", { ascending: false }).limit(1);
+
+  if (searchParams.series) {
+    if (searchParams.series === "noaa") {
+      passesBuilder.eq("is_noaa", true);
+    } else if (searchParams.series === "meteor") {
+      passesBuilder.eq("is_meteor", true);
+    }
+  }
+
+  const { data } = await passesBuilder;
   const [latestPass] = data ?? [];
 
   return <Pirousel latestPass={latestPass} />;
