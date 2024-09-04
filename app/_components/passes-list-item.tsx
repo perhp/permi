669fcabe5d44d203671c8d8543e66a3c42bf5b46
Per Hansen <me@perhp.com>
2024-09-04 14:31:22 +0200

    Revert "feat: use transform api to decrease load times"
    This reverts commit b0bbc402b3e1819a7bf7b212a6eeac995157d0a0.

--- app/_components/passes-list-item.tsx
+++ app/_components/passes-list-item.tsx
@@ -22,7 +22,7 @@ export default function PassesListItem({ pass }: Props) {
         <div className="flex h-52 md:h-72 lg:h-80 bg-black rounded-xl relative">
           <div className="w-5 h-5 rounded-full border-t-2 border-l-2 border-t-white border-l-white border-r-2 border-b-2 border-r-white border-b-white/25 animate-spin absolute z-0 inset-0 m-auto" />
           <Image
-            src={`${CDN_URL}/images/${imagesWithoutGraphs[0].path}?width=400&height=400&resize=cover`}
+            src={`${CDN_URL}/images/${imagesWithoutGraphs[0].path}`}
             alt={pass.images[0]!.path.split(".")[0].replace("-", " ")}
             width={300}
             height={300}
--- lib/cdn-url.ts
+++ lib/cdn-url.ts
@@ -1,1 +1,1 @@
-export const CDN_URL = "https://htyouocguzgigopxxidt.supabase.co/storage/v1/render/image/public/passes";
+export const CDN_URL = "https://htyouocguzgigopxxidt.supabase.co/storage/v1/object/public/passes";
