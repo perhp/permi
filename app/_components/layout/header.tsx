77de864ab5a0a14f5cdcdf3952756890074d68d3
Per Hansen <me@perhp.com>
2026-07-15 18:10:24 +0200

    fix: github icon

--- app/_components/layout/header.tsx
+++ app/_components/layout/header.tsx
@@ -1,4 +1,5 @@
-import { Github, SatelliteDish } from "lucide-react";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { SatelliteDish } from "lucide-react";
 import Link from "next/link";
 import { NavLink } from "./nav-link";
 
@@ -7,7 +8,8 @@ export default function Header() {
     <header className="relative border-b border-gray-100 z-10 bg-white">
       <div className="flex items-center justify-between container h-14">
         <Link href="/" className="flex items-center font-bold gap-1.5">
-          <SatelliteDish className="w-5 h-5" /> <span className="hidden sm:inline">permi</span>
+          <SatelliteDish className="w-5 h-5" />{" "}
+          <span className="hidden sm:inline">permi</span>
         </Link>
         <nav>
           <ul className="flex gap-5 items-center">
@@ -17,8 +19,12 @@ export default function Header() {
             <NavLink href="/setup" className="font-medium text-sm">
               Setup
             </NavLink>
-            <NavLink href="https://github.com/perhp/permi" target="_blank" className="bg-gray-100 hover:bg-gray-100/75 p-2 rounded-full">
-              <Github className="w-5 h-5" />
+            <NavLink
+              href="https://github.com/perhp/permi"
+              target="_blank"
+              className="bg-gray-100 hover:bg-gray-100/75 p-2 rounded-full"
+            >
+              <GitHubLogoIcon className="w-5 h-5" />
             </NavLink>
           </ul>
         </nav>
